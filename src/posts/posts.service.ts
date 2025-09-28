f742841c31c34005b40ce4291b61374dfb3156f4
Guiziin227 <gh63388@gmail.com>
2025-09-28 10:59:53 -0300

    Adiciona controle de autorização ao serviço de posts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,24 +2,46 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { CaslAbilityService } from '../casl/casl-ability/casl-ability.service';
+import { accessibleBy } from '@casl/prisma';
 
 @Injectable()
 export class PostsService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService,
+              private abilityService: CaslAbilityService) {}
 
   create(createPostDto: CreatePostDto & { authorId: string }) {
+
+    const ability = this.abilityService.ability
+
+    if(!ability.can('create', 'Post')){
+      throw new Error('Unauthorized to create post')
+    }
+
     return this.prisma.post.create({
       data: createPostDto,
     });
   }
 
-  findAll() {
-    return this.prisma.post.findMany();
+   findAll() {
+
+    const ability = this.abilityService.ability
+
+    const posts =  this.prisma.post.findMany({
+      where:{
+        AND: [accessibleBy(ability, 'read').Post]
+      }
+    });
+    return posts
   }
 
   findOne(id: string) {
+
+    const ability = this.abilityService.ability
     return this.prisma.post.findUnique({
-      where: { id },
+      where: { id,
+        AND: [accessibleBy(ability, 'read').Post]
+      },
     });
   }
 
